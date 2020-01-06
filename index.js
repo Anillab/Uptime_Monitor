90b914cd23924a4cdf557c19e61f20f18afa0988
Anillab <anillabusolo@gmail.com>
2020-01-06 23:15:04 +0000

    defined a function to take requests and listen on aserver

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 
 var http=require('http');
 var url=require('url');
+// server responds to requests with strings
+var server=http.createServer(function(req,res){
+  // Get the url and parse it
+  var parsedUrl=url.parse(req.url,true)
+  // Get the path
+  var path =parsedUrl.pathname;
+  var trimmedPath=path.replace(/^\/+|\/+$/g,'');
+  // Get the query string as an object
+  var queryStringObject=parsedUrl.query
+  // Get the Http Method
+   var method =req.method.toLowerCase();
+   // Get the headers of the object
+   var headers =req.headers;
+  // Send the response
+  res.end('Hello World\n');
+  // log the request path
+  console.log('Request received with these headers:' ,headers )
+
+});
+// start the server and have it listrn on port 3000
+server.listen(3000,function(){
+  console.log('The server is listening on port 3000')
+})
